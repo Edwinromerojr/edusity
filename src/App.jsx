7533b40c4bd5fcbf567ca322553ea7327a6575a7
unknown <edwinrmrjr@gmail.com>
2024-09-03 18:43:12 +0800

    About and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our PROGRAM' Title='What We Offer'/>
         <Programs />
+        <About />
+        <Title subTitle='Gallery' Title='Campus Photos'/>
       </div>
     </div>
   )
