6d75472a7a3a2725ad77d9cec267ab8f7ce7d493
unknown <edwinrmrjr@gmail.com>
2024-09-03 22:33:26 +0800

    testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Title subTitle='Gallery' Title='Campus Photos'/>
         <Campus />
         <Title subTitle='TESTIMONIALS' Title='What Student Says'/>
+        <Testimonials />
       </div>
     </div>
   )
