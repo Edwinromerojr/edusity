a3ece4fbf4e7cffae863d54ba64863536080b8a8
unknown <edwinrmrjr@gmail.com>
2024-09-09 14:24:48 +0800

    footer

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -26,7 +26,6 @@ const Contact = () => {
     if (data.success) {
       setResult("Form Submitted Successfully");
       event.target.reset();
-      event.target.reset();
     } else {
       console.log("Error", data);
       setResult(data.message);
